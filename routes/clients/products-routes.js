b3926cffe85ba0a738bfba08f66dab023faa755d
thanhlamcode <lamdoan1122334455@gmail.com>
2024-06-29 13:49:19 +0700

    Cấu hình folder controller

--- routes/clients/products-routes.js
+++ routes/clients/products-routes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../../controller/products-controller");
 
-router.get("/", (req, res) => {
-  res.render("client/pages/products/index");
-});
+router.get("/", controller.index);
 
 module.exports = router;
