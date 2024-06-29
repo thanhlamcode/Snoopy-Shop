57d7bb6cd70cf074c5cd3b65021b6f418b9cfa9c
thanhlamcode <lamdoan1122334455@gmail.com>
2024-06-29 21:43:22 +0700

    Nhúng bootstrap - cấu hình file tĩnh

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.set("view engine", "pug");
 //routes
 routes(app);
 
+app.use(express.static("public"));
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
