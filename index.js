a6a909a6c3ee2acc0067ea35f4f1ab1225a6d398
thanhlamcode <lamdoan1122334455@gmail.com>
2024-07-13 21:55:45 +0700

    Config deploy

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ app.use(flash());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
-app.use(express.static("public"));
+app.use(express.static(`${__dirname}/public`));
 
-app.set("views", "./views");
+app.set("views", `${__dirname}/views`);
 app.set("view engine", "pug");
 
 //routes
