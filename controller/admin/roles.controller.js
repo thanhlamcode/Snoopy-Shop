278a00b60e4e7191a0c67bcf07aa6a2b382f43e7
thanhlamcode <lamdoan1122334455@gmail.com>
2024-07-22 15:44:19 +0700

    Xây dựng trang nhóm quyền

---
+++ controller/admin/roles.controller.js
@@ -0,0 +1,32 @@
+const Role = require("../../models/role.model");
+const systemAdmin = require("../../config/systems");
+
+// [GET] /admin/roles
+module.exports.index = async (req, res) => {
+  let find = {
+    deleted: false,
+  };
+
+  const records = await Role.find(find);
+
+  res.render("admin/pages/roles/index", {
+    pageTitle: "Nhóm quyền",
+    records: records,
+  });
+};
+
+// [GET] /admin/roles/create
+module.exports.create = (req, res) => {
+  res.render("admin/pages/roles/create", {
+    pageTitle: "Tạo nhóm quyền",
+  });
+};
+
+// [POST] /admin/roles/create
+module.exports.createPost = async (req, res) => {
+  console.log(req.body);
+
+  const records = Role(req.body);
+  await records.save();
+  res.redirect(`${systemAdmin.prefitAdmin}/roles`);
+};
--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -2,10 +2,12 @@ const systemAdmin = require("../../config/systems");
 const dashboardRouter = require("./dashboard.route");
 const productsRouter = require("./products.route");
 const productsCategoryRouter = require("./products-category.route");
+const roleRouter = require("./roles.route");
 
 module.exports = (app) => {
   const PATCH_ADMIN = systemAdmin.prefitAdmin;
   app.use(PATCH_ADMIN + "/dashboard", dashboardRouter);
   app.use(PATCH_ADMIN + "/products", productsRouter);
   app.use(PATCH_ADMIN + "/products-category", productsCategoryRouter);
+  app.use(PATCH_ADMIN + "/roles", roleRouter);
 };
