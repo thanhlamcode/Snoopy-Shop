2ce2ca6b662d2107026785f81e4c8f34a3f293b8
thanhlamcode <lamdoan1122334455@gmail.com>
2024-08-06 15:43:53 +0700

    Sản phẩm nổi bật

--- controller/clients/home-controller.js
+++ controller/clients/home-controller.js
@@ -1,6 +1,25 @@
+const Product = require("../../models/products.model");
+
 // [GET] /
-module.exports.index = (req, res) => {
+module.exports.index = async (req, res) => {
+  const products = await Product.find({
+    deleted: false,
+    status: "active",
+    featured: "1",
+  }).limit(6);
+
+  products.forEach((item) => {
+    const newPrice = Math.round(
+      (item.price * (100 - item.discountPercentage)) / 100
+    );
+    item.newPrice = newPrice;
+    const discountPrice = item.price - item.newPrice;
+    item.discountPrice = discountPrice;
+  });
+  // console.log(products);
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    products: products,
   });
 };
