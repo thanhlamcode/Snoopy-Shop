2b54e2a3513d65bda84c86afa55349c481932258
thanhlamcode <lamdoan1122334455@gmail.com>
2024-08-06 09:11:55 +0700

    Hiển thị menu chung bên clients

---
+++ middleware/client/category.middleware.js
@@ -0,0 +1,18 @@
+const ProductCategory = require("../../models/products-category");
+const treeHelper = require("../../helpers/createTree");
+
+module.exports.category = async (req, res, next) => {
+  try {
+    const find = {
+      deleted: false,
+    };
+
+    const records = await ProductCategory.find(find);
+    const layout = treeHelper.tree(records);
+    res.locals.layout = layout;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/clients/index-routes.js
+++ routes/clients/index-routes.js
@@ -1,7 +1,10 @@
 const productsRouter = require("./products-routes");
 const homeRouter = require("./home-routes");
+const categoryMiddleware = require("../../middleware/client/category.middleware");
 
 module.exports = (app) => {
+  app.use(categoryMiddleware.category);
+
   app.use("/", homeRouter);
 
   app.use("/products", productsRouter);
